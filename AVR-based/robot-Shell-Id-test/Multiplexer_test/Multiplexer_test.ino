8dade5c3e4448b1985c4dd661f578e7c198b54d0
lucask18 <lkatayama3@gatech.edu>
2020-10-18 13:55:58 -0400

    modified code to only test sensor 2

--- AVR-based/robot-Shell-Id-test/Multiplexer_test/Multiplexer_test.ino
+++ AVR-based/robot-Shell-Id-test/Multiplexer_test/Multiplexer_test.ino
@@ -23,15 +23,15 @@ void setup(void)
   Serial.begin(9600);
   
   /* Initialise the 1st sensor */
-  tcaselect(2);
-  if(!cSensor1.begin())
-  {
-    Serial.println("cSensor1 was not identified");
-    while(1);
-  }
+  //tcaselect(3);
+  //if(!cSensor1.begin())
+  //{
+   // Serial.println("cSensor1 was not identified");
+   // while(1);
+  //}
   
   /* Initialise the 2nd sensor */
-  tcaselect(6);
+  tcaselect(7);
   if(!cSensor2.begin())
   {
     Serial.println("cSensor2 was not identified");
@@ -46,33 +46,28 @@ void loop(void)
   float red, green, blue;
   /* Get a new sensor event */ 
   
-  tcaselect(2);
+  //tcaselect(2);
  
-  Serial.print("Sensor #1 - ");
+  //Serial.print("Sensor #1 - ");
   
-  cSensor1.setInterrupt(false);  // turn on LED
 
-  delay(60);  // takes 50ms to read
+  //delay(60);  // takes 50ms to read
 
-  cSensor1.getRGB(&red, &green, &blue);
+  //cSensor1.getRGB(&red, &green, &blue);
   
-  cSensor1.setInterrupt(true);  // turn off LED
 
-  Serial.print("R:\t"); Serial.print(int(red)); 
-  Serial.print("\tG:\t"); Serial.print(int(green)); 
-  Serial.print("\tB:\t"); Serial.print(int(blue));
-  Serial.print("\n");
+  //Serial.print("R:\t"); Serial.print(int(red)); 
+  //Serial.print("\tG:\t"); Serial.print(int(green)); 
+  //Serial.print("\tB:\t"); Serial.print(int(blue));
+  //Serial.print("\n");
   
-  tcaselect(6);
+  tcaselect(7);
   Serial.print("Sensor #2 - ");
   
-  cSensor2.setInterrupt(false);  // turn on LED
 
   delay(60);  // takes 50ms to read
 
   cSensor2.getRGB(&red, &green, &blue);
-  
-  cSensor2.setInterrupt(true);  // turn off LED
 
   Serial.print("R:\t"); Serial.print(int(red)); 
   Serial.print("\tG:\t"); Serial.print(int(green)); 
