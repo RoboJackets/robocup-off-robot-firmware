af77106184dc3293d3a2266b92e1c16d2103f98a
Arvind Srinivasan <arvindsrinivasan@gatech.edu>
2021-02-10 00:09:19 -0500

    Forces the interrupts to be sequential and only on falling edge triggers

--- AVR-based/ballspeed_v2.0/ballspeed_v2.0.ino
+++ AVR-based/ballspeed_v2.0/ballspeed_v2.0.ino
@@ -1,99 +1,80 @@
-// the idea of the code is to get data from the movement of a ball as it passes four sensors. 
-//  the code intends to get the time the ball passes by every sensor, including when it comes in and out of it.
-//  it also calculates the mean speed, accelaration, and net force acting on it
+// the idea of the code is to get data from the movement of a ball as it passes four sensors.
+// the code intends to get the time the ball passes by every sensor, including when it comes in and out of it.
+// it also calculates the mean speed, and potentially could calculate the acceleration, and net force acting on it
 
-// max speed the ball can reach is ~6m/s. for an spacing of 5cm between sensors it will take <8.333ms for the ball to reach the next sensor. 
-//    with 10μs periods, the percent error would be 0.036.
+// max speed the ball can reach is ~6m/s. for an spacing of 5cm between sensors it will take <8.333ms for the ball to reach the next sensor.
+// with 10μs periods, the percent error would be 0.036.
 
-const int r_sensors = 0.05; //distance between sensors (m)
-const int r_beam = 0.000001; //IDK, check the used component data sheet for actual size of light beam ~this value (1μm) is a guess~
-const int mass_ball = 45.93*(10^-3); // mass of the golf ball (kg)
+const double r_sensors = 0.1; //distance between sensors (m)
+const double mass_ball = 45.93 * (10 ^ -3); // mass of the golf ball (kg)
 
-const int sensor1 = 1; //TX -> sensor 1
-const int sensor2 = 0;//RX -> sensor 2
-const int sensor3 = 2; //SDA -> sensor 3
-const int sensor4 = 3; //SCL -> sensor 4
+const int sensor1 = 3; //TX -> sensor 1
+const int sensor2 = 2; //RX -> sensor 2
+const int sensor3 = 0; //SDA -> sensor 3
+const int sensor4 = 1; //SCL -> sensor 4
 
-unsigned long time_sensor[8]; //time sensor was triggered (μs)
-int i = 0;
-int k = 0;
+unsigned long time_sensor[4]; //time sensor was triggered (μs)
+int j;
+double mean_velocity;
+boolean passAllSensors[] = {false, false, false, false};
 
-float v_rsensor[3];
-float v_rbeam[4];
-float vmean;
-float a;
-float fnet_friction;
+void setup() {
+  Serial.begin(115200);
 
-const int ledPin =  LED_BUILTIN;
-const long interval = 0.001;  //interval for led blink (miliseconds)
-int ledState = LOW; 
+  pinMode(sensor1, INPUT);
+  pinMode(sensor2, INPUT);
+  pinMode(sensor3, INPUT);
+  pinMode(sensor4, INPUT);
 
-void setup() {  
-  pinMode(LED_BUILTIN, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(sensor1), interrupt1, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(sensor2), interrupt2, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(sensor3), interrupt3, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(sensor4), interrupt4, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(sensor1), interrupt1, FALLING);
+  attachInterrupt(digitalPinToInterrupt(sensor2), interrupt2, FALLING);
+  attachInterrupt(digitalPinToInterrupt(sensor3), interrupt3, FALLING);
+  attachInterrupt(digitalPinToInterrupt(sensor4), interrupt4, FALLING);
 }
 
 // reading sensors and saving times ball passes
 void loop() {
-  if (ledState == 1){
-    digitalWrite(ledPin, ledState);
-    delay(100);
-    ledState = LOW;
-  }
-  if (i==8){
+  if (passAllSensors[0] && passAllSensors[1] && passAllSensors[2] && passAllSensors[3] ) {
     calculations();
-   
   }
 }
 // an interrupt for each sensor
-void interrupt1 (){
-  time_sensor[i] = micros();
-  ledState = HIGH;
-  i++;
+void interrupt1 () {
+  noInterrupts();
+  time_sensor[0] = micros();
+  passAllSensors[0] = true;
+  interrupts();
 }
-void interrupt2 (){
-  time_sensor[i] = micros();
-  ledState = HIGH;
-  i++;
+void interrupt2 () {
+  noInterrupts();
+  time_sensor[1] = micros();
+  passAllSensors[1] = true;
+  interrupts();
 }
-void interrupt3 (){
-  time_sensor[i] = micros();
-  ledState = HIGH;
-  i++;
+void interrupt3 () {
+  noInterrupts();
+  time_sensor[2] = micros();
+  passAllSensors[2] = true;
+  interrupts();
 }
-void interrupt4 (){
-  time_sensor[i] = micros();
-  ledState = HIGH;
-  i++;
+void interrupt4 () {
+  noInterrupts();
+  time_sensor[3] = micros();
+  passAllSensors[3] = true;
+  interrupts();
 }
 
 // calculating variables
-void calculations(){
-  for (int j=0; j<8; j+=2) {
-    
-    if (k<3){ //v_rsensor has size 3, and v_rbeam size 4, then, in the last loop, we shouldn't record a velocity for v_rsensor.
-      
-      v_rsensor[k] = r_sensors/((time_sensor[j+2]-time_sensor[j])*10^-6);
-      v_rbeam[k] = r_beam/((time_sensor[j+1]-time_sensor[j])*10^-6);
-      vmean += v_rsensor[k]+v_rbeam[k];
-    } 
-    else{
-    
-      v_rbeam[k] = r_beam/((time_sensor[j+1]-time_sensor[j])*10^-6);
-      vmean += v_rbeam[k];
-    } 
-    
-    k++;
-  }
-  
-  vmean = (vmean)/7;
-  a = (v_rbeam[3]-v_rsensor[0])/((time_sensor[6]-time_sensor[7])*10^-6);
+void calculations() {
+  mean_velocity = 0.0;
 
- Serial.print("Mean speed: ");
- Serial.print(vmean);
- Serial.print("\nMean accelaration: ");
- Serial.print(a);
+  for (j = 0; j < 3; j++) {
+    mean_velocity += r_sensors / (0.000001 * (time_sensor[j + 1] - time_sensor[j]));
+  }
+
+  mean_velocity /= 3;
+
+  Serial.print("Mean speed (m/s): ");
+  Serial.println(mean_velocity, 5);
+  memset(passAllSensors, 0, sizeof(passAllSensors));
 }
