311886e23ab77f4708f35ad405be87e8c73e6395
Arvind Srinivasan <arvindsrinivasan@gatech.edu>
2021-02-16 23:10:00 -0500

    I hope this works

--- AVR-based/ballspeed_v2.0/ballspeed_v2.0.ino
+++ AVR-based/ballspeed_v2.0/ballspeed_v2.0.ino
@@ -2,21 +2,18 @@
 // the code intends to get the time the ball passes by every sensor, including when it comes in and out of it.
 // it also calculates the mean speed, and potentially could calculate the acceleration, and net force acting on it
 
-// max speed the ball can reach is ~6m/s. for an spacing of 5cm between sensors it will take <8.333ms for the ball to reach the next sensor.
-// with 10μs periods, the percent error would be 0.036.
+// max speed the ball can reach is ~12m/s. for an spacing of 19.05cm between sensors it will take <15.875ms for the ball to reach the next sensor.
 
 const double r_sensors = 0.1905; //distance between sensors (m)
-const double mass_ball = 45.93 * (10 ^ -3); // mass of the golf ball (kg)
 
-const int sensor1 = 3; //TX -> sensor 1
-const int sensor2 = 2; //RX -> sensor 2
-const int sensor3 = 0; //SDA -> sensor 3
-const int sensor4 = 1; //SCL -> sensor 4
+#define sensor1 3 //TX -> sensor 1
+#define sensor2 2 //RX -> sensor 2
+#define sensor3 0 //SDA -> sensor 3
+#define sensor4 1 //SCL -> sensor 4
 
 unsigned long time_sensor[4]; //time sensor was triggered (μs)
 int j;
 double mean_velocity;
-boolean passAllSensors[] = {false, false, false, false};
 
 void setup() {
   Serial.begin(115200);
@@ -34,33 +31,30 @@ void setup() {
 
 // reading sensors and saving times ball passes
 void loop() {
-  if (passAllSensors[0] && passAllSensors[1] && passAllSensors[2] && passAllSensors[3] ) {
+  if (time_sensor[0] && (time_sensor[3] > time_sensor[2]) && (time_sensor[2] > time_sensor[1]) && (time_sensor[1] > time_sensor[0])) {
     calculations();
   }
 }
+
 // an interrupt for each sensor
 void interrupt1 () {
   noInterrupts();
   time_sensor[0] = micros();
-  passAllSensors[0] = true;
   interrupts();
 }
 void interrupt2 () {
   noInterrupts();
   time_sensor[1] = micros();
-  passAllSensors[1] = true;
   interrupts();
 }
 void interrupt3 () {
   noInterrupts();
   time_sensor[2] = micros();
-  passAllSensors[2] = true;
   interrupts();
 }
 void interrupt4 () {
   noInterrupts();
   time_sensor[3] = micros();
-  passAllSensors[3] = true;
   interrupts();
 }
 
@@ -75,6 +69,10 @@ void calculations() {
   mean_velocity /= 3;
 
   Serial.print("Mean speed (m/s): ");
-  Serial.println(mean_velocity, 4);
-  memset(passAllSensors, 0, sizeof(passAllSensors));
+  Serial.println(mean_velocity, 3);
+  Serial.println(time_sensor[0]);
+  Serial.println(time_sensor[1]);
+  Serial.println(time_sensor[2]);
+  Serial.println(time_sensor[3]);
+  memset(time_sensor, 0, sizeof(time_sensor));
 }
