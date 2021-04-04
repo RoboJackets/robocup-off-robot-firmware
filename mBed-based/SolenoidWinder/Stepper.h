3333df79e6e430c6e65ba2d17a424e2f2342d08f
shreyachalla <71445563+shreyachalla@users.noreply.github.com>
2021-04-04 16:08:01 -0400

    Update Stepper.h

--- mBed-based/SolenoidWinder/Stepper.h
+++ mBed-based/SolenoidWinder/Stepper.h
@@ -7,7 +7,7 @@ class Stepper {
         int motorStepsPerRev;
         //Factor of microstepping, default is 1 with no microstepping
         int microstepFactor;
-        // default direction
+        // default direction 
         int dir;
 
         //Objects of DigitalOut class to control motor motion and direction
@@ -16,11 +16,11 @@ class Stepper {
 
         // Method returns the period in seconds
         float rpsToPeriod();
-
-
+        
+        
 
     public:
-
+        
         // Constructor for stepper motor
         Stepper(float rps, PinName speedPin, PinName dirPin, int microstepFactor = 1, int motorStepsPerRev = 200, int dir = 0 );
 
@@ -30,21 +30,20 @@ class Stepper {
 
         // Getter for motorStepsPerRev
         int getmotorStepsPerRev() {return motorStepsPerRev; }
-        void setMotorStepsPerRev(int motorStepsPerRev) {this-> motorStepsPerRev = motorStepsPerRev;}
 
          // Getter for microstepFactor
-        int getMicrostepFactor() {return microstepFactor;}
-        void setMicrostepFactor(int microstepFactor) {this-> microstepFactor = microstepFactor;}
-
-        // Getter and setter for direction
-        void setDir(int dir) {this-> dir = dir;}
-        int getDir() {return dir;}
+        int getMicrostepFactor() {return microstepFactor;} 
+        
+        // Getter and setter for direction 
+        void setDir(int dir) {this-> dir = dir;} 
+        int getDir() {return dir;} 
 
         // Method reverses the direction of the motor after given waitTime in seconds
-        void reverseMotor(int waitTime);
+        // void reverseMotor(int waitTime);
 
         // Method turns motor in a certain direction (default is 0) for a given runTime in seconds
-        void TurnMotor(int runTime);
+        void TurnMotor(int runTime); 
+        void TurnMotor(int runTime, Stepper motor1);
 
 
         } ;
