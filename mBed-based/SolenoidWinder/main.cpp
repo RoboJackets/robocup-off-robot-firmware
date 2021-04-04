9ce911f18a6ed3887f6148308be1b0055473f754
shreyachalla <71445563+shreyachalla@users.noreply.github.com>
2021-04-04 16:04:39 -0400

    Update main.cpp

--- mBed-based/SolenoidWinder/main.cpp
+++ mBed-based/SolenoidWinder/main.cpp
@@ -2,7 +2,9 @@
 #include "Stepper.h"
 
 int main() {
+    
+        Stepper Motor1(0.25, p22, p21, 32);  
+        Stepper Motor2(0.25, p23, p24, 32); 
+        Motor2.TurnMotor(100, Motor1);
+     } 
 
-        Stepper Motor1(0.25, p21, p22, 32);
-        Motor1.TurnMotor(100);
-     }
