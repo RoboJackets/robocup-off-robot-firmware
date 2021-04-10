f6ee684bba1732228e76fdf4814381dc04885552
shreyachalla <71445563+shreyachalla@users.noreply.github.com>
2021-04-10 12:28:25 -0400

    Test cases for turning 2 motors

--- mBed-based/SolenoidWinder/main.cpp
+++ mBed-based/SolenoidWinder/main.cpp
@@ -3,8 +3,23 @@
 
 int main() {
     
-        Stepper Motor1(0.25, p22, p21, 32);  
-        Stepper Motor2(0.25, p23, p24, 32); 
-        Motor2.TurnMotor(100, Motor1);
+        Stepper motor1 (0.5, p22, p21, 32);
+        Stepper motor2 (0.5, p23, p24, 32); 
+        
+        // 1000 iterations of 0.1 seconds for Motor 2 
+        int count = 0;
+        while (count < 1000) {
+            
+            motor2.turnMotor(0.1);
+            count += 1; 
+        }
+        
+        // 1000 iterations of 0.1 seconds for Motor 1 and Motor 2 
+        /* count = 0;
+        while (count < 1000) {
+            motor1.turnMotor(0.1); 
+            motor2.turnMotor(0.1);
+            count += 1; 
+        } */ 
+    
      } 
-
